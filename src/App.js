275ebe4ae88e5f7b42a757dacc72acc3f39e719b
xyzjesus <cryptopunk4201@senthy.com>
2022-09-11 13:14:34 +0000

    strike the repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import fleekLogo from './fleek-logo.png';
-import reactLogo from './react-logo.svg';
 
 import './App.css';
 
@@ -8,11 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={fleekLogo} className="Fleek-logo" alt="fleek-logo" />
-        <span className="Big-plus">+</span>
-        <img src={reactLogo} className="React-logo" alt="react-logo" />
-        <p>
-          React App deployed on IPFS through Fleek.
+        <p className="gut">
+          gut! devs
         </p>
         <a
           className="App-link"
